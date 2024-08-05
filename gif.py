fbc6c623310a279eefc8a6f014bcaf9ca38fa354
redstars72 <169080970+redstars72@users.noreply.github.com>
2024-08-05 17:21:45 +0100

    gif.py

--- gif.py
+++ gif.py
@@ -1,10 +1,17 @@
 import cv2,os
-
+from PIL import Image
 path="D:\\elini\\Google Drive EliNicholls2\\Jet Learn\\Visual Studio Code Files\\openCV\\pixilart-frames"
 os.chdir(path)
+print(os.listdir(path))
+for file in os.listdir(path):
+    if file.endswith(".png") or file.endswith(".jpeg") or file.endswith(".jpg"):
+        img=Image.open(os.path.join(path,file))
+        resimg=img.resize((150,150))
+        resimg.save(file,"PNG")
 merchr="merrychristmas.avi"
 mcvid=cv2.VideoWriter(merchr,0,2,(150,150))
 for file in os.listdir(path):
-    print(file)
-    image=cv2.imread(os.path.join(path,file))
-    mcvid.write(image)
+    if file.endswith(".png") or file.endswith(".jpeg") or file.endswith(".jpg"):
+        print(file)
+        image=cv2.imread(os.path.join(path,file))
+        mcvid.write(image)
